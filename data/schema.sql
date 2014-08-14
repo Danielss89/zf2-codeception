cad98f36ac8a0e3468e74779c99c6fb18806ff9e
Danielss89 <Danielss89@gmail.com>
2014-08-15 00:03:00 +0200

    Codeception suites setup

--- data/schema.sql
+++ data/schema.sql
@@ -7,3 +7,6 @@ CREATE TABLE `user`
     `password`      VARCHAR(128) NOT NULL,
     `state`         SMALLINT UNSIGNED
 ) ENGINE=InnoDB CHARSET="utf8";
+
+INSERT INTO `user` (`user_id`, `username`, `email`, `display_name`, `password`, `state`) VALUES
+(1, NULL, 'test@test.com', NULL, '$2y$14$zG3OABCDjmDZ/bZ6f2fROeFPRzhJQ0PdLZn5fGDWjl1FlDF/JiNWS', NULL);
--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,9 @@
-/* Replace this file with actual dump of your database */
+CREATE TABLE `user`
+(
+    `user_id`       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username`      VARCHAR(255) DEFAULT NULL UNIQUE,
+    `email`         VARCHAR(255) DEFAULT NULL UNIQUE,
+    `display_name`  VARCHAR(50) DEFAULT NULL,
+    `password`      VARCHAR(128) NOT NULL,
+    `state`         SMALLINT UNSIGNED
+) ENGINE=InnoDB CHARSET="utf8";
